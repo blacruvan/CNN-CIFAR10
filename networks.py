5f713986ac430b4543fc43dae7698c7387d857b6
root <root@DESKTOP-D7Q84KK>
2024-05-23 23:13:52 +0200

    Nuevos test

--- networks.py
+++ networks.py
@@ -71,3 +71,23 @@ class NN04(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+class NN05(nn.Module):
+    def __init__(self, out_1 = 32, out_2 = 64, number_of_classes = 10):
+        super(NN05, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels = 3, out_channels = out_1, kernel_size = 5, padding = 2)
+        self.maxpool1 = nn.MaxPool2d(kernel_size = 2)
+        self.cnn2 = nn.Conv2d(in_channels = out_1, out_channels = out_2, kernel_size = 5, padding = 2)
+        self.maxpool2 = nn.MaxPool2d(kernel_size = 2)
+        self.fc1 = nn.Linear(out_2 * 8 * 8, number_of_classes)
+        
+    def forward(self, x):
+        x = self.cnn1(x)
+        x = torch.relu(x)
+        x = self.maxpool1(x)
+        x = self.cnn2(x)
+        x = torch.relu(x)
+        x = self.maxpool2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return(x)
